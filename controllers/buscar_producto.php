a87a9235b84a8db01406c05869dfd16b59029be7
Lucas Miliauskas <miliauskaslm@gmail.com>
2019-06-09 21:31:21 -0300

    Se agrega busqueda de productos

---
+++ controllers/buscar_producto.php
@@ -0,0 +1,58 @@
+<?php
+require_once "../models/product.php";
+$conexion = new Conexion();
+$conexion = $conexion->conectar();
+$key_word = $conexion->real_escape_string($_GET['key_word']);
+
+// Obtiene productos que tengan la palabra clave en el titulo, descripcion o subdescripcion
+$sql = "SELECT p.id, u.email, p.title, p.description, p.subDescription, p.price, p.create_date
+        FROM product AS p
+        INNER JOIN usuario AS u ON p.idUser = u.id
+        WHERE p.title LIKE '%$key_word%' OR p.description LIKE '%$key_word%' OR p.subDescription LIKE '%$key_word%';";
+if (!$result = $conexion->query($sql)) {
+    return die("Ha ocurrido un error al ejecutar la consulta");
+}
+
+$productos = array();
+while ($row = $result->fetch_assoc()) {
+    $subCategories = array();
+    $images = array();
+    $productId = $row['id'];
+    // Obtiene las subcategorias del producto
+    $sql = "SELECT sc.name AS subCategory, c.name AS category
+            FROM sub_category_product AS scp
+            INNER JOIN subcategory AS sc ON scp.idSubCategory = sc.id
+            INNER JOIN category AS c ON sc.idCategory = c.id
+            WHERE scp.idProduct = $productId;";
+    if (!$categories = $conexion->query($sql)) {
+        return die("Ha ocurrido un error al ejecutar la consulta");
+    }
+    while ($category = $categories->fetch_assoc()) {
+        $subCategories[$category['subCategory']] = $category['category'];
+    }
+
+    // Obtiene las imagenes del producto
+    $sql = "SELECT image
+            FROM product_image
+            WHERE product_id = $productId;";
+    if (!$img = $conexion->query($sql)) {
+        return die("Ha ocurrido un error al ejecutar la consulta");
+    }
+    while ($image = $img->fetch_assoc()) {
+        array_push($images, $image['image']);
+    }
+
+    $productData = array();
+    $productData['id'] = $productId;
+    $productData['idUser'] = $row['email'];
+    $productData['title'] = $row['title'];
+    $productData['description'] = $row['description'];
+    $productData['subDescription'] = $row['subDescription'];
+    $productData['price'] = $row['price'];
+    $productData['categories'] = $subCategories;
+    $productData['images'] = $images;
+    $productData['create_date'] = $row['create_date'];
+    $product = new Product($productData);
+    array_push($productos, $product);
+}
+return $productos;
--- resources/templates/navbar.php
+++ resources/templates/navbar.php
@@ -1,39 +1,40 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #82E0FF;">
-    <div class="col-sm-1">
-  <a class="navbar-brand" href="home.php">
-    <img src="../resources/img/logo.png" width="32" height="35" class="d-inline-block align-top" alt="">
-  </a>
-</div>
-<div class="col-sm-2">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-
-    </ul>
+  <div class="col-sm-1">
+    <a class="navbar-brand" href="home.php">
+      <img src="../resources/img/logo.png" width="32" height="35" class="d-inline-block align-top" alt="">
+    </a>
   </div>
-</div>
-<div class="col-sm-6">
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" style="width:80%;" type="search" placeholder="Buscar" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-  </form>
-</div>
-<div class="col-sm-3">
-  <div class="d-flex justify-content-end" style="padding-right:50px;">
-<ul class="navbar-nav">
-  <?php
-  session_start();
-  if (isset($_SESSION['email'])) {
+  <div class="col-sm-2">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <form class="form-inline my-2 my-lg-0" method="GET" action="../views/resultado_busqueda.php">
+      <input name="key_word" class="form-control mr-sm-2" style="width:80%;" type="search" placeholder="Buscar"
+        aria-label="Search" value="<?php echo isset($key_word) ? $key_word : ''; ?>">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+  <div class="col-sm-3">
+    <div class="d-flex justify-content-end" style="padding-right:50px;">
+      <ul class="navbar-nav">
+        <?php
+session_start();
+if (isset($_SESSION['email'])) {
     echo '<li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
       aria-haspopup="true" aria-expanded="false">
@@ -47,17 +48,17 @@
       <a class="dropdown-item" href="../controllers/logout.php">Cerrar sesion</a>
     </div>
   </li>';
-  } else {
+} else {
     echo '<li class="nav-item active">
     <a class="nav-link" href="../views/registrar.php">Registrarse</a>
   </li>
   <li class="nav-item active">
     <a class="nav-link" href="../views/ingresar.php">Iniciar sesion</a>
   </li>';
-  }
-  ?>
+}
+?>
 
-</ul>
-</div>
-</div>
+      </ul>
+    </div>
+  </div>
 </nav>
